838abb734bea9464b0f619e25d132a2c203b8b81
Janina Peterek <jpeterek@google.com>
2019-09-05 14:22:22 +0300

    mr cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
 root to: "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :cocktails, only: [:index, :show, :new, :create] do
@@ -7,5 +6,5 @@ resources :cocktails, only: [:index, :show, :new, :create] do
   resources :reviews, only: [:create]
 end
 resources :doses, only: [:destroy]
-resources :review, only: [:destroy]
+resources :reviews, only: [:destroy]
 end
